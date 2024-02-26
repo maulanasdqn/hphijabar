a1d4d7eda685599de586fc8dd330e9c84ef9196f
Fenny Oktaviani <foktvn04@gmail.com>
2024-02-26 12:08:22 +0700

    Improvement : Hamburger menu on mobile mode

--- app/(landing)/_modules/banner.tsx
+++ app/(landing)/_modules/banner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Banner: FC = (): ReactElement => {
   return (
-    <section className="flex lg:flex-row  flex-col justify-between items-center lg:p-0 p-8 lg:pt-0 pt-32 h-screen">
+    <section className="flex lg:flex-row flex-col lg:justify-between justify-center gap-y-8 items-center lg:p-0 p-8 h-screen">
       <div className="flex flex-col lg:gap-6 gap-2 w-full font-semibold lg:text-5xl text-3xl text-primary-2">
         <span>Himpunan Perawat</span>
         <span>Holistik Indonesia</span>
