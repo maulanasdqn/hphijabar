fe9edae6ad63059da446329c39ef2a65e48310ef
Maulana Sodiqin <maulanasdqn@gmail.com>
2024-02-27 20:58:30 +0700

    feat: slug news

--- app/listnews/[slug]/page.tsx
+++ app/listnews/[slug]/page.tsx
@@ -1,24 +1,25 @@
 "use client";
 import { NextPage } from "next";
 import { ReactElement } from "react";
-import { useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import Image from "next/image";
+import { Dummy_News } from "../store";
 
 const DetailNewsPage: NextPage = (): ReactElement => {
-  const router = useRouter();
+  const { slug } = useParams();
   return (
     <section className="flex flex-col bg-white w-full h-full py-4 p-8 lg:p-0 lg:pt-24 pt-24 justify-center">
       <span className="flex text-white lg:text-3xl text-md font-bold bg-primary-2 rounded-md w-full text-center items-center justify-center h-14">
-        Kunjungan ke Labschool, Menpora Coba Kipin ATM
+        {Dummy_News.find((news) => news.slug === slug)?.title}
       </span>
       <Image
         alt="news"
         className="w-full h-[400px] "
-        src="/news3.png"
+        src={Dummy_News.find((news) => news.slug === slug)?.src as string}
         width={1000}
         height={1000}
       />
-      <p>detail</p>
+      <p>{Dummy_News.find((news) => news.slug === slug)?.detail}</p>
     </section>
   );
 };
